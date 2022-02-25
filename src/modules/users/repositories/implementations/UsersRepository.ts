2b71f02c5dd083dcc7fac3504059186621f4169f
OJailson17 <JaylsonO17@gmail.com>
2022-02-25 20:39:57 -0300

    feat: add create new user feat

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,37 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = {
+      name,
+      email,
+      admin: false,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
+  turnAdmin(receivedUser: User): User | null {
+    // !Change final return removing null
+    const getUser = this.users.find((user) => user === receivedUser);
+    console.log(getUser);
+
+    return null;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,16 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    // Check if user exists
+    const emailExists = this.usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new Error("Email already exists");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
