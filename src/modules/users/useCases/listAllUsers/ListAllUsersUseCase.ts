8f7e0140829ad5cf01292c53822b2604af010ffd
OJailson17 <JaylsonO17@gmail.com>
2022-02-25 21:20:24 -0300

    feat: add list all users feat

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,23 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    console.log(user_id);
+
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    const isUserAdmin = user.admin === true;
+
+    if (!isUserAdmin) {
+      throw new Error("User is not an admin");
+    }
+
+    const allUsers = this.usersRepository.list();
+
+    return allUsers;
   }
 }
 
